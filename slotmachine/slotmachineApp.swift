43d7aad0385770ec27eb5e5b6771607240bdc90a
Colin Snyder <colin7snyder@gmail.com>
2021-02-23 20:47:18 -0800

    Initial Commit

---
+++ slotmachine/slotmachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotmachineApp.swift
+//  slotmachine
+//
+//  Created by Colin Snyder on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct slotmachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
